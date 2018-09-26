851832881e471ed795d591db690d44f66006e39a
egor066416 <066416@gmail.com>
2018-09-26 20:07:12 +0300

    subscible style

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -8,28 +8,16 @@ export default (props) => {
       <p className="subscribe__title">Subscribe to Newsletter</p>
       <form
         className="subscribe__form"
-        // onSubmit={(e) => addSubscribe(e)}
+      // onSubmit={(e) => addSubscribe(e)}
       >
         <input type="email"
           placeholder="Enter your email"
         />
-        <button type="submit">Subscribe now</button>
+        <button
+          className="button red"
+          type="submit"
+        >Subscribe now</button>
       </form>
     </div>
-
   )
 }
-{/*     <form className="wrapper">
-      <div class="input-group mb-3">
-        <input type="email"
-          className="form-control"
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2" />
-        <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="button">Button</button>
-        </div>
-      </div>
-    </form> */ }
-
-
